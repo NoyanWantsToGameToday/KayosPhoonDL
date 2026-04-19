c54f1553b01259165622c71d2b6cb33833305d87
Noyan <noyannowhasyt@gmail.com>
2026-04-19 10:53:34 +0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -33,15 +33,6 @@ export default {
                                 <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
                             </button>
                         </td>
-                        <td class="rank">
-                            <p v-if="i + 1 <= 25" class="type-label-lg">#{{ i + 1 }}</p>
-                            <p v-else class="type-label-lg">Extended</p>
-                        </td>
-                        <td class="level" :class="{ 'active': selected == i, 'error': !level }">
-                            <button @click="selected = i">
-                                <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
-                            </button>
-                        </td>
                     </tr>
                 </table>
             </div>
